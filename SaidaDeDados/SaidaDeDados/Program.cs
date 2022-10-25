6ea116b099c17f7afffe4cb1bf517b514e3683ba
lucas <lucasdokko@hotmail.com>
2022-10-24 21:18:40 -0300

    saidaDeDados

---
+++ SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados {
+    class Program {
+        static void Main(string[] args) {
+
+            double saldo = 10.35784;
+            int idade = 32;
+            string nome = "Maria";
+
+            //PlaceHolders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            //concatenação,
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a "+ saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+    } 
+
+}
+
