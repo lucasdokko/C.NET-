ccfb874474625bcec6897c01f5c3a9fe246c0343
lucas <lucasdokko@hotmail.com>
2022-10-25 01:07:21 -0300

    ImplicitaeCasting

---
+++ conversãoImplicitaeCasting/conversãoImplicitaeCasting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace operadoresDeAtribuicao {
+    class program {
+        static void Main(String[] args) {
+
+            float x = 4.5f;
+
+
+            double y = x; // houve uma converção implicita pos o y é double e recebeu uma variavel x que é float
+
+            Console.WriteLine(y);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            double a;
+            float b;
+
+            a = 5.1;
+            b = (float)a; // assim que define um casting
+
+            Console.WriteLine(b);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            double c;
+            int d;
+
+            c = 5.1;
+            d = (int)a; //Havera perda de informação pos a variavel aceita apenas inteiro e como o numero é quebrado
+
+            Console.WriteLine(d);
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            int z = 5;
+            int w = 2;
+
+            double resultado =(double) z / w;
+
+            Console.WriteLine(resultado);
+            
+
+
+
+        }
+    }
+}
