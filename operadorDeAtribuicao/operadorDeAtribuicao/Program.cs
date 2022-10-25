9c41609e166e4f27293a6fad5b127bd7d90b54af
lucas <lucasdokko@hotmail.com>
2022-10-25 00:40:07 -0300

    ..

---
+++ operadorDeAtribuicao/operadorDeAtribuicao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace operadoresDeAtribuicao {
+    class program {
+        static void Main(String[] args) {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            int b = 10;
+            b++;
+            Console.WriteLine(b);
+
+            b--;
+            Console.WriteLine(b);
+
+            int c = ++b;  //Primeiro incrementa o valor 1 e depois adiona o valor da variavel ficando com valor 11
+            Console.WriteLine(c);
+
+            c = --b;
+            Console.WriteLine(c); // primeiro adiciona o vlaor da variavel e depois adiciona o valor 1, mas o valor continuara 10 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
