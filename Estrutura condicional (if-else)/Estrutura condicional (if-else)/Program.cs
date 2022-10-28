1912f15b1849192b92b64612a0431cb559eb9e91
lucas <lucasdokko@hotmail.com>
2022-10-27 23:40:58 -0300

    Estrutural condicional if-else

---
+++ Estrutura condicional (if-else)/Estrutura condicional (if-else)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace operadorescomparativos {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com um número inteiro:");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine("Par!");
+            } else {
+                Console.WriteLine("Impar!");
+
+
+            }
+
+
+            Console.WriteLine("Qual hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12) {
+                Console.WriteLine("Bom Dia !");
+            } else if (hora < 18) {
+                Console.WriteLine("Boa Tarde!");
+            } else {
+                Console.WriteLine("Boa noite");
+            }
+
+        }
+    }
+}
